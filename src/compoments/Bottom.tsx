1e6337f0fc9903d616ca08626c32f5ab355b2638
huayifeng <461249104@qq.com>
2020-10-23 00:19:53 +0800

    fix: doc

--- src/compoments/Bottom.tsx
+++ src/compoments/Bottom.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Bottom() {
     return (
         <div className="bottom">
-            © 2020–2021 – <a href="https://qiufeng.blue" target="_blank" >qiufeng</a> 微信(wechat): qiufengble
+            © 2020–2021 – <a href="https://qiufeng.blue" target="_blank" >qiufeng</a> 微信(wechat): qiufengblue
         </div>
     )
 }
